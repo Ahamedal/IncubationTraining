1fb0532dae5b16578d907005d57509413f2e8cad
Ahamed ali <94349140+Ahamedal@users.noreply.github.com>
2022-06-25 20:30:19 +0530

    Add files via upload

---
+++ calltaxibooking/TaxiHistory.java
@@ -0,0 +1,57 @@
+package calltaxibooking;
+
+import java.util.Date;
+
+public class TaxiHistory {
+ private int bookingId;
+ private int customerId;
+ private char pickUpPoint;
+ private char dropPoint;
+ private long pickUpTime;
+ private int amount;
+public int getBookingId() {
+	return bookingId;
+}
+public void setBookingId(int bookingId) {
+	this.bookingId = bookingId;
+}
+public int getCustomerId() {
+	return customerId;
+}
+public void setCustomerId(int customerId) {
+	this.customerId = customerId;
+}
+public char getPickUpPoint() {
+	return pickUpPoint;
+}
+public void setPickUpPoint(char pickUpPoint) {
+	this.pickUpPoint = pickUpPoint;
+}
+public char getDropPoint() {
+	return dropPoint;
+}
+public void setDropPoint(char dropPoint) {
+	this.dropPoint = dropPoint;
+}
+public long getPickUpTime() {
+	return pickUpTime;
+}
+public void setPickUpTime(long pickUpTime) {
+	this.pickUpTime = pickUpTime;
+}
+
+public int getAmount() {
+	return amount;
+}
+public void setAmount(int amount) {
+	this.amount = amount;
+}
+@Override
+public String toString() {
+	Date da=new Date(pickUpTime);
+	return "[bookingId=" + bookingId + ", customerId=" + customerId + ", pickUpPoint=" + pickUpPoint
+			+ ", dropPoint=" + dropPoint + ", pickUpTime=" + da + ", amount=" +amount+"]";
+}
+ 
+ 
+}
