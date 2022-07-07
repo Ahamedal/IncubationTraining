5a38537e076c0547d51cee0020a138f664f05cdc
Ahamed ali <94349140+Ahamedal@users.noreply.github.com>
2022-07-07 20:03:28 +0530

    Add files via upload

--- Project/src/main/webapp/Customer.js
+++ Project/src/main/webapp/Customer.js
@@ -19,10 +19,19 @@ function display(){
 	var customer=new Customer(nam,addres,mobileNumbe);
 	array.push(customer);
 	
-	document.getElementById("title").innerHTML="<tr>"+"<th> name </th>"+"<th> address </th>"+"<th> mobile number </th></tr></table>";
+	document.getElementById("title").innerHTML="<tr>"+"<th> name </th>"+"<th> address </th>"+"<th> mobile number </th><th> Delete </th></tr></table>";
 	
 	for(var i=0;i<array.length;i++){
-		document.getElementById("title").innerHTML+="<tr><td>"+array[i].name+"</td>"+"<td>"+array[i].address+"</td>"+"<td>"+array[i].mobileNumber+"</td></tr>";
+		var btn = document.createElement('input');
+		btn.type = "button";
+		btn.className = "btn";
+		
+		
+		document.getElementById("title").innerHTML+="<tr><td>"+array[i].name+"</td>"+"<td>"+array[i].address+"</td>"+"<td>"+array[i].mobileNumber+"</td><td>"+document.body.appendChild(btn);
+		document.getElementById("bb"+i).innerHTML="Submit";+"</td></tr>";
 	}
 	
+
 }
+
+
